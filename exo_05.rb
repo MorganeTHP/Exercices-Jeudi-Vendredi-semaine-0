26a6bc5fd022cff7069d7ed0e3937287842b9199
Morgane <twixigame9@gmail.com>
2018-10-18 15:49:15 +0200

    Jeudi/les réponses aux questions sont dans le README

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+#Affiche ce qu'il y a entre guillemets puts "On va compter le nombre d'heures de travail à THP"
+#Affiche Travails : puis le résultat de l'opération puts "Travail : #{10 * 5 * 11}"
+# Affiche le texte puis le résultat de l'opération puts "En minutes ça fait : #{10 * 5 * 11 * 60}" 
+
+#Affiche le texte puts "Et en secondes ?"
+
+#Affiche le résultat puts 10 * 5 * 11 * 60 * 60
+
+#Affiche le texte dont l'opération puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#Affiche la résultat de l'opération en booléen puts 3 + 2 < 5 - 7
+
+#Affiche la question avec l'opération puis le résultat puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#Affiche la question avec l'opération puis le résultat puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#Affiche le texte puts "Ok, c'est faux alors !"
+
+#Affiche le texte puts "C'est drôle ça, faisons-en plus :"
+
+#Affiche le texte evc les chiffres puis une réponse en booléen puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#Idem que la ligne 20 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#idem que la ligne 20 et 21 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
